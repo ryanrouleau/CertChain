148c007e3d2721a9104655873e3257eafd02c40c
Jake Crawford <jake.crawford@outlook.com>
2018-02-24 20:45:36 -0700

    Add untested request code

--- certgen/certgen.py
+++ certgen/certgen.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_file
 import ssllib
+from zipfile import ZipFile
+import json
+import urllib2
 
 app = Flask(__name__)
 
@@ -10,13 +13,15 @@ def cert_gen():
 @app.route('/insert', methods=['POST'])
 def insertCerts():
     result = request.form 
-    print result
+
     ca_cert, server_cert, server_private_key = ssllib.generate_certs_and_keys(result.get('country'), result.get('state'), result.get('city'), result.get('commonname'), result.get('org'), result.get('orgunit'))
 
-    with open('cacert.crt', 'w') as f:
-        f.write(ca_cert.as_pem())
-    with open('cert.crt', 'w') as f:
+    cert_name = result.get('commonname') + '.crt'
+    with open(cert_name, 'w') as f:
         f.write(server_cert.as_pem())
-        f.write(server_private_key.as_pem(None))
+        f.write(server_private_key.as_pem(None)) 
 
-    return redirect(url_for('cert_gen')) 
+    # insert ca_cert into blockchain here
+    req = urllib2.urlopen('localhost:PORT/put?domain=' + commonname + '&fileContents=' + server_cert).read()
+
+    return send_file(cert_name, as_attachment=True)
